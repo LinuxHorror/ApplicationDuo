0580fb8710263d59545d79e298cbfa0ba27783cd
Egor.Efimenko <20_Morokey_04@mail.ru>
2023-11-18 21:21:03 +0300

    Added ModelAttribute into GetMapping method

--- src/main/java/com/example/applicationduo/web/RegistrationController.java
+++ src/main/java/com/example/applicationduo/web/RegistrationController.java
@@ -1,8 +1,10 @@
 package com.example.applicationduo.web;
 
+import com.example.applicationduo.dto.UserCreationDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -13,10 +15,12 @@ public class RegistrationController {
 
     public static final String REG_PAGE = "registration";
     @GetMapping
-    public ModelAndView regPage(){
+    public ModelAndView regPage(@ModelAttribute(name="newUser") UserCreationDto user){
         var modelAndView = new ModelAndView(REG_PAGE);
         return modelAndView;
     }
+
+
 
 
 }
