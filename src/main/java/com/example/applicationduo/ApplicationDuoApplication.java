6c6a2ea303d26aabc6e9121a7ac1bcb57dad19ee
Vlad.Zherko <vladikon19@tut.by>
2023-11-18 20:07:25 +0300

    init

---
+++ src/main/java/com/example/applicationduo/ApplicationDuoApplication.java
@@ -0,0 +1,13 @@
+package com.example.applicationduo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicationDuoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApplicationDuoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/applicationduo/ApplicationDuoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.applicationduo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationDuoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
