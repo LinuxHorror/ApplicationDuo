1daf784cacb711eab4a2abad8afd74f35781fa7c
Vlad.Zherko <vladikon19@tut.by>
2023-11-18 22:09:41 +0300

    save post to enter controller

--- src/main/java/com/example/applicationduo/web/EnterPageController.java
+++ src/main/java/com/example/applicationduo/web/EnterPageController.java
@@ -1,18 +1,39 @@
 package com.example.applicationduo.web;
 
 
+import com.example.applicationduo.dao.UserDao;
 import com.example.applicationduo.dto.UserCreationDto;
+import com.example.applicationduo.mappers.UserMapper;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+
+@RequiredArgsConstructor
 
 @Controller
 @RequestMapping("/enter")
 public class EnterPageController {
+
+    private final UserMapper userMapper;
+    private final UserDao repository;
     @GetMapping
     public ModelAndView getMainPage(@ModelAttribute("newUser") UserCreationDto dto){
         return new ModelAndView("enterPage");
+    }
+
+    @PostMapping("/save")
+    public ModelAndView enterData(@Valid @ModelAttribute("newUser") UserCreationDto dto, BindingResult result){
+        var model = new ModelAndView("enterPage");
+        if(!result.hasFieldErrors()){
+            // TODO DAO
+            model.addObject("newUser", new UserCreationDto());
+        }
+        return model;
     }
 }
