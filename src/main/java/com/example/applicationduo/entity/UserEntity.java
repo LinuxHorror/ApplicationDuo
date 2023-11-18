874c9b61a1f9b6437676a5004c2eae1759c50b4a
Egor.Efimenko <20_Morokey_04@mail.ru>
2023-11-18 20:57:13 +0300

    Added Builder, CreationTimestamp, UpdateTimestamp annotations

--- src/main/java/com/example/applicationduo/entity/UserEntity.java
+++ src/main/java/com/example/applicationduo/entity/UserEntity.java
@@ -2,17 +2,21 @@ package com.example.applicationduo.entity;
 
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
 import java.util.Date;
 import java.util.UUID;
 
 @Entity
-@Table(name = "Users")
+@Table(name = "users")
 @NoArgsConstructor
 @AllArgsConstructor
 @Data
+@Builder
 public class UserEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
@@ -20,7 +24,9 @@ public class UserEntity {
     private String username;
     private String email;
     private String password;
+    @CreationTimestamp
     private Date dateOfCreation;
+    @UpdateTimestamp
     private Date dateOfUpdate;
     private Integer version;
 }
