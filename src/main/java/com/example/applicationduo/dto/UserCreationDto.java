6c1c6580e64baef6481113d77cc6003c5a169c2f
Egor.Efimenko <20_Morokey_04@mail.ru>
2023-11-18 20:56:35 +0300

    Added Builder annotation

--- src/main/java/com/example/applicationduo/dto/UserCreationDto.java
+++ src/main/java/com/example/applicationduo/dto/UserCreationDto.java
@@ -1,6 +1,7 @@
 package com.example.applicationduo.dto;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -8,6 +9,7 @@ import java.util.UUID;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
 public class UserCreationDto {
     private UUID id;
     private String username;
