bfa28bd27285619f0a3da4436c9e6b42c68f184c
Vlad.Zherko <vladikon19@tut.by>
2023-11-19 14:41:09 +0300

    new method

--- src/main/java/com/example/applicationduo/repositories/UserRepository.java
+++ src/main/java/com/example/applicationduo/repositories/UserRepository.java
@@ -3,7 +3,9 @@ package com.example.applicationduo.repositories;
 import com.example.applicationduo.entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface UserRepository extends JpaRepository<UserEntity, UUID> {
+    Optional<UserEntity> findByEmailAndPassword(String email, String password);
 }
