6c8c5fac0762362d6da516a7c772f58d5797ce81
Egor.Efimenko <20_Morokey_04@mail.ru>
2023-11-21 20:50:51 +0300

    Changed Optional to List

--- src/main/java/com/example/applicationduo/repositories/ProductRepository.java
+++ src/main/java/com/example/applicationduo/repositories/ProductRepository.java
@@ -3,11 +3,12 @@ package com.example.applicationduo.repositories;
 import com.example.applicationduo.entity.ProductEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 
 public interface ProductRepository extends JpaRepository<ProductEntity, Integer> {
 
     //TODO native? query desc order
-    Optional<ProductEntity> findByProductTitle(String name);
+    List<ProductEntity> findByProductTitle(String name);
 }
