162b6bdda09449c0933547b44fbe187539978059
Egor.Efimenko <20_Morokey_04@mail.ru>
2023-11-24 21:04:08 +0300

    AddToCart method created

--- src/main/java/com/example/applicationduo/web/controllers/StoreController.java
+++ src/main/java/com/example/applicationduo/web/controllers/StoreController.java
@@ -1,20 +1,39 @@
 package com.example.applicationduo.web.controllers;
 
 
+import com.example.applicationduo.dto.ProductDto;
+import com.example.applicationduo.mappers.ProductMapper;
+import com.example.applicationduo.service.ProductService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
-
+@RequiredArgsConstructor
 @Controller
 @RequestMapping("/store")
 public class StoreController {
-    @GetMapping("/shoppingCart")
+    private final ProductService productService;
+    private final ProductMapper productMapper;
+    @GetMapping("/cart")
     public ModelAndView shoppingCart(){
-        return new ModelAndView("shoppingCartPage");
+        return new ModelAndView("cartPage");
     }
     @GetMapping
-    public ModelAndView mainPage(){
-        return new ModelAndView("mainPage");
+    public ModelAndView mainPage(@ModelAttribute("product") ProductDto productDto){
+        var modelAndView = new ModelAndView("storePage");
+        modelAndView.addObject("product", productMapper.toListDto(productService.findAll()));
+        return modelAndView;
     }
+
+    @PostMapping("/addToCart")
+    public ModelAndView addToCart(@ModelAttribute("newProduct") ProductDto product){
+        var modelAndView = new ModelAndView();
+
+        return new ModelAndView("redirect:/store");
+    }
+
+
 }
