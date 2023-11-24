2f302acd14146129007bf27cdcef4eb0413e94a2
Vlad.Zherko <vladikon19@tut.by>
2023-11-24 22:44:58 +0300

    added : comparator

---
+++ src/main/java/com/example/applicationduo/web/controllers/CartController.java
@@ -0,0 +1,44 @@
+package com.example.applicationduo.web.controllers;
+
+
+import com.example.applicationduo.entity.ProductEntity;
+import com.example.applicationduo.mappers.ProductMapper;
+import com.example.applicationduo.service.ProductService;
+import com.example.applicationduo.utils.ProductComparator;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.Collections;
+import java.util.List;
+
+@RequiredArgsConstructor
+@Controller
+@RequestMapping("*/cart")
+public class CartController {
+
+    private final ProductService service;
+    private final ProductMapper mapper;
+    @GetMapping
+    public ModelAndView shoppingCart(){
+        ModelAndView modelAndView = new ModelAndView("cartPage");
+        List<ProductEntity> allProducts = service.findAll();
+        modelAndView.addObject("products", mapper.toListDto(allProducts));
+        float sumOfPurchase = 0;
+        for(ProductEntity product : allProducts){
+            sumOfPurchase += product.getCount() * product.getPrice();
+        }
+        modelAndView.addObject("totalPurchase", sumOfPurchase);
+        return modelAndView;
+    }
+    @GetMapping("/sort")
+    public ModelAndView getSortedPage(){
+        ModelAndView modelAndView = shoppingCart();
+        List<ProductEntity> all = service.findAll();
+        all.sort(new ProductComparator());
+        modelAndView.addObject("products", mapper.toListDto(all));
+        return modelAndView;
+    }
+}
