3cfdc60e50655964fc4a6225db64fc8da253142a
Vlad.Zherko <vladikon19@tut.by>
2023-11-24 20:13:13 +0300

    shows data

--- src/main/java/com/example/applicationduo/web/controllers/StoreController.java
+++ src/main/java/com/example/applicationduo/web/controllers/StoreController.java
@@ -1,20 +1,31 @@
 package com.example.applicationduo.web.controllers;
 
 
+import com.example.applicationduo.mappers.ProductMapper;
+import com.example.applicationduo.service.ProductService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+@RequiredArgsConstructor
+
 @Controller
 @RequestMapping("/store")
 public class StoreController {
-    @GetMapping("/shoppingCart")
+    private final ProductService service;
+    private final ProductMapper mapper;
+
+    @GetMapping("/cart")
     public ModelAndView shoppingCart(){
-        return new ModelAndView("shoppingCartPage");
+        ModelAndView modelAndView = new ModelAndView("cartPage");
+        modelAndView.addObject("products", mapper.toListDto(service.findAll()));
+        return modelAndView;
     }
     @GetMapping
     public ModelAndView mainPage(){
         return new ModelAndView("mainPage");
+
     }
 }
