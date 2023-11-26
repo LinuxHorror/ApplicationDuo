aac32eb13808cbc10daee9ed717bfd6050a85e9d
Egor.Efimenko <20_Morokey_04@mail.ru>
2023-11-26 15:36:40 +0300

    Added functionality

--- src/main/java/com/example/applicationduo/mappers/CartMapper.java
+++ src/main/java/com/example/applicationduo/mappers/CartMapper.java
@@ -1,4 +1,24 @@
 package com.example.applicationduo.mappers;
 
+import com.example.applicationduo.dto.CartDto;
+import com.example.applicationduo.entity.CartEntity;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+@Mapper(
+        componentModel = "spring"
+)
 public interface CartMapper {
+
+    @Mapping(target = "id", source = "id")
+    @Mapping(target = "productId", source = "productId")
+    @Mapping(target = "count", source = "count")
+    CartEntity toEntity(CartDto dto);
+
+    @Mapping(target = "id", source = "id")
+    @Mapping(target = "productId", source = "productId")
+    @Mapping(target = "count", source = "count")
+    CartDto toDto(CartEntity entity);
+
+
 }
