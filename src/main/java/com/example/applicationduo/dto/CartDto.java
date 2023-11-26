a2f7487e1efa40f4f532bb38986611425c3b36e5
Egor.Efimenko <20_Morokey_04@mail.ru>
2023-11-26 15:37:17 +0300

    Added fields

--- src/main/java/com/example/applicationduo/dto/CartDto.java
+++ src/main/java/com/example/applicationduo/dto/CartDto.java
@@ -1,5 +1,19 @@
 package com.example.applicationduo.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class CartDto {
     private Integer id;
+
+    private Integer productId;
+
+    private Integer count;
+
 }
